aef45ff93eb2fcb278a5ad9ae0d7b25849d3e368
unknown <ethan.sean.friedman@gmail.com>
2022-01-28 18:57:25 -0500

    added get user posts to profile backend

--- src/main/java/com/web/socket/websocket/Repositories/PostRepository.java
+++ src/main/java/com/web/socket/websocket/Repositories/PostRepository.java
@@ -3,6 +3,8 @@ package com.web.socket.websocket.Repositories;
 import com.web.socket.websocket.model.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PostRepository extends JpaRepository<Post, Long> {
+import java.util.Optional;
 
+public interface PostRepository extends JpaRepository<Post, Long> {
+    Optional<Post> findByUsername(String username);
 }
--- src/main/java/com/web/socket/websocket/controller/PostController.java
+++ src/main/java/com/web/socket/websocket/controller/PostController.java
@@ -5,13 +5,11 @@ import com.web.socket.websocket.model.Post;
 import com.web.socket.websocket.payload.response.MessageResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class PostController {
@@ -21,6 +19,11 @@ public class PostController {
     @GetMapping("/getposts")
     public List<Post> getPosts() {
         return postRepository.findAll();
+    }
+
+    @GetMapping("/getposts/{username}")
+    public Optional<Post> getPostsByUser(@PathVariable String username) {
+        return postRepository.findByUsername(username);
     }
 
     @PostMapping("/createpost")
